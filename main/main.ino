4a308979130f296114527268f60c5ad812770507
claaj <matias.cajal@hotmail.com>
2023-06-19 17:15:36 -0300

    Agregar comentarios linea por linea explicando que hace el codigo.

--- main/main.ino
+++ main/main.ino
@@ -39,38 +39,42 @@ uint8_t medirdistancia(){
 
 void setup() {
   setup_asm();
-  cabezal.attach(10);   //Asigno el pin 10 al servo (corresponde a la salida SERVO_1 del shield)
-  cabezal.write(*R17);  //Función que escribe un ángulo al servomotor. 90 es mirando al frente
+  cabezal.attach(10);         //Asigno el pin 10 al servo (corresponde a la salida SERVO_1 del shield)
+  cabezal.write(*R17);        //Función que escribe un ángulo al servomotor. 90 es mirando al frente
   // FOWARD = 1, BACKWARD = 2, BRAKE = 3
   // Ver https://github.com/adafruit/Adafruit-Motor-Shield-library/blob/d81504057474e940de7f0a0934f19c626846a154/AFMotor.h#L108-L109
-  M1.run(FORWARD);      //Asigno direccion al motor1
-  M2.run(FORWARD);      //Asigno direccion al motor2
+  M1.run(FORWARD);           //Asigno direccion al motor1
+  M2.run(FORWARD);           //Asigno direccion al motor2
 }
 
 void loop() {
-  servo_centro();
+  servo_centro();           // Se pone el servo en el centro en el 90° (R17).
   cabezal.write(*R17);
-  avanzar();
-  M1.setSpeed(*R19);
+  avanzar();                // Se setea los registros R19 y R20 para que los motores avanzan.
+  M1.setSpeed(*R19);        
   M2.setSpeed(*R20);
 
-  *R21=medirdistancia();  //Uso de la funcion medir distancia y guardado en R21
-  check_distancia();
+  *R21=medirdistancia();    // Uso de la funcion medir distancia y guardado en R21
+  check_distancia();        // Checkea que si la distancia es mayor a 30, se coloca un 0 en R16, de lo contrario se pone un 1.
 
   if (*R16 == 1 || (*R07 < 4 && *R07 > 0)) {
-    frenar();
+    frenar();               // Se setea la velocidad de los motores en 0 para que no se desplace el auto.
     M1.setSpeed(*R19);
     M2.setSpeed(*R20);
     delay_250();
 
-    mirar_dos_lados(cabezal);
-    esquivar();
-    M1.setSpeed(*R19);
-    M2.setSpeed(*R20);
+    mirar_dos_lados(cabezal);  // Se llama a mirar lados y se pasa como argumento cabezal que controla servo. 
+    esquivar();                // Se llama a la función esquivar.
+    M1.setSpeed(*R19);         // Esquivar termina seteando valores para los motores.
+    M2.setSpeed(*R20);         // Dependiendo de la logica se setean determinado valores.
     delay_250();
   }
 }
 
+/*
+Función que controla gira el servo a la derecha y escribe en el R10 la distancia.
+Luego gira el servo a la izquierda y guarda en R09 la distancia izquierda.
+*/
 void mirar_dos_lados(Servo cabezal) {
   servo_derecha();
   cabezal.write(*R17);
